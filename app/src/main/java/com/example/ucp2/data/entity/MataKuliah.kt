a7650f0514d9a1eadddf46cdf44af8f1b869e085
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-18 10:20:28 +0700

    Membuat tabel dengan nama "mata_kuliah" beserta parameternya

--- app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
+++ app/src/main/java/com/example/ucp2/data/entity/MataKuliah.kt
@@ -1,3 +1,15 @@
 package com.example.ucp2.data.entity
 
-data class MataKuliah()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "mata_kuliah")
+data class MataKuliah(
+    @PrimaryKey
+    val kode: String,
+    val nama: String,
+    val sks: String,
+    val semester: String,
+    val jenis: String,
+    val dosenPengampu: String
+)
