6e155229270e2911a3205d748eec856f592de27e
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-18 11:04:52 +0700

    menambahkan interface RepositoryDosen beserta fungsinya

--- app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryDosen.kt
@@ -1,4 +1,9 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.Dosen
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryDosen {
+    suspend fun insertDosen(dosen: Dosen)
+    fun getAllDosen() : Flow<List<Dosen>>
 }
