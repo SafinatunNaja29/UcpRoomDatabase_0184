54f95805bcaaf01bac71f90cb2b0cf8b5238ca87
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-18 10:43:00 +0700

    menambahkan interface MataKuliahDao berserta metodenya

--- app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
+++ app/src/main/java/com/example/ucp2/data/dao/MataKuliahDao.kt
@@ -1,4 +1,28 @@
 package com.example.ucp2.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface MataKuliahDao {
+    @Insert
+    suspend fun insertMataKuliah(mataKuliah: MataKuliah)
+
+    @Query("SELECT * FROM mata_kuliah ORDER BY nama ASC")
+    suspend fun getAllMataKuliah(): Flow<List<MataKuliah>>
+
+    @Query("SELECT * FROM mata_kuliah WHERE kode = :kode")
+    suspend fun getMataKuliah(kode: String): MataKuliah
+
+    @Update
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
+
+    @Delete
+    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
+
 }
