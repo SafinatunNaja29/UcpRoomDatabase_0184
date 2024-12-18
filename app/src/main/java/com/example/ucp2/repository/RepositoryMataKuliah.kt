f5180a0661b4beecf51e7d54f14458f9a7beedc1
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-18 11:08:41 +0700

    menambahkan interface RepositoryMataKuliah beserta fungsinya

--- app/src/main/java/com/example/ucp2/repository/RepositoryMataKuliah.kt
+++ app/src/main/java/com/example/ucp2/repository/RepositoryMataKuliah.kt
@@ -1,4 +1,15 @@
 package com.example.ucp2.repository
 
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
 interface RepositoryMataKuliah {
+    suspend fun insertMataKuliah(mataKuliah: MataKuliah)
+    fun getAllMataKuliah() : Flow<List<MataKuliah>>
+
+    fun getMatakuliah(kode: String) : Flow<MataKuliah>
+
+    suspend fun deleteMataKuliah(mataKuliah: MataKuliah)
+
+    suspend fun updateMataKuliah(mataKuliah: MataKuliah)
 }
