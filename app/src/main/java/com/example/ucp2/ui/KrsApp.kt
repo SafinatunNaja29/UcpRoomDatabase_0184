13a0b03e217992906119ef385104407c1dda91ca
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 21:55:25 +0700

    menambahkan code pada file KrsApp

--- app/src/main/java/com/example/ucp2/ui/KrsApp.kt
+++ app/src/main/java/com/example/ucp2/ui/KrsApp.kt
@@ -1,4 +1,14 @@
 package com.example.ucp2.ui
 
-class KrsApp {
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
+
+
+class KrsApp : Application() {
+    lateinit var containerApp: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+
+        containerApp = ContainerApp(this)
+    }
 }
