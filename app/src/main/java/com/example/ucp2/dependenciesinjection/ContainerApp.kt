fd9fe33c622a0ceb39f706a65cedf1dbb6ffabb4
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 21:53:08 +0700

    menambahkan code pada file ContainerApp

--- app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2/dependenciesinjection/ContainerApp.kt
@@ -1,2 +1,25 @@
 package com.example.ucp2.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2.data.database.DatabaseApp
+import com.example.ucp2.repository.LocalRepositoryDosen
+import com.example.ucp2.repository.LocalRepositoryMataKuliah
+import com.example.ucp2.repository.RepositoryDosen
+import com.example.ucp2.repository.RepositoryMataKuliah
+
+interface InterfaceContainerApp {
+    val repositoryDosen: RepositoryDosen
+
+    val repositoryMK: RepositoryMataKuliah
+}
+
+class ContainerApp(private val context: Context) : InterfaceContainerApp {
+    override val repositoryDosen: RepositoryDosen by lazy {
+        LocalRepositoryDosen(DatabaseApp.getDatabase(context).dosenDao())
+    }
+
+    override val repositoryMK: RepositoryMataKuliah by lazy {
+        LocalRepositoryMataKuliah(DatabaseApp.getDatabase(context).matakuliahDao())
+    }
+
+}
