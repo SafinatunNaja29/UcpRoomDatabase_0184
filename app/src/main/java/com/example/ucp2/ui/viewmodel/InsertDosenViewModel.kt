eb72192d42c3c8497d765af117cff22de43f529c
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 22:50:06 +0700

    Menambahkan logika untuk validasi input, penyimpanan data dosen, dan pengaturan pesan snackbar

--- app/src/main/java/com/example/ucp2/ui/viewmodel/InsertDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertDosenViewModel.kt
@@ -1,6 +1,58 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.repository.RepositoryDosen
+import kotlinx.coroutines.launch
+
+class InsertDosenViewModel(private val repositoryDosen: RepositoryDosen) : ViewModel() {
+    var uiState by mutableStateOf(DosenUIState())
+
+    fun updateState(dosenEvent: DosenEvent) {
+        uiState = uiState.copy(
+            dosenEvent = dosenEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.dosenEvent
+        val errorState = FormErrorState(
+            nidn = if (event.nidn.isNotEmpty()) null else "NIDN tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenisKelamin = if (event.jenisKelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong"
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData(){
+        val currentEvent = uiState.dosenEvent
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryDosen.insertDosen(currentEvent.toDosenEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        dosenEvent = DosenEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(snackBarMessage = "Data gagal disimpan")
+                }
+            }
+        } else{
+            uiState = uiState.copy(snackBarMessage = "Input tidak valid. Periksa kembali data Anda.")
+        }
+    }
+
+    fun resetSnackBarMessage(){
+        uiState = uiState.copy(snackBarMessage = null)
+    }
+}
 
 data class DosenUIState(
     val dosenEvent: DosenEvent = DosenEvent(),
