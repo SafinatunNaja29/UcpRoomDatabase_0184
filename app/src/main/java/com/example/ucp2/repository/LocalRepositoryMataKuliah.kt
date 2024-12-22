93df92e211888faefb1a7495e3f84dab59b70a4d
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 21:30:48 +0700

    menambahkan LocalRepositoryMataKuliah untuk pengelolaan data Mata Kuliah

--- app/src/main/java/com/example/ucp2/repository/LocalRepositoryMataKuliah.kt
+++ app/src/main/java/com/example/ucp2/repository/LocalRepositoryMataKuliah.kt
@@ -1,4 +1,29 @@
 package com.example.ucp2.repository
 
-class LocalRepositoryMataKuliah {
+import com.example.ucp2.data.dao.MataKuliahDao
+import com.example.ucp2.data.entity.MataKuliah
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryMataKuliah (
+    private val mataKuliahDao: MataKuliahDao
+) : RepositoryMataKuliah {
+    override suspend fun insertMataKuliah(mataKuliah: MataKuliah) {
+        mataKuliahDao.insertMataKuliah(mataKuliah)
+    }
+
+    override fun getAllMataKuliah(): Flow<List<MataKuliah>> {
+        return mataKuliahDao.getAllMataKuliah()
+    }
+
+    override fun getMataKuliah(kode: String): Flow<MataKuliah> {
+        return mataKuliahDao.getMataKuliah(kode)
+    }
+
+    override suspend fun deleteMataKuliah(mataKuliah: MataKuliah) {
+        mataKuliahDao.deleteMataKuliah(mataKuliah)
+    }
+
+    override suspend fun updateMataKuliah(mataKuliah: MataKuliah) {
+        mataKuliahDao.updateMataKuliah(mataKuliah)
+    }
 }
