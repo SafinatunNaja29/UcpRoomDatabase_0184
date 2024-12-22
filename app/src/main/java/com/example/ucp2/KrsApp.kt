408365933504877afe4870bf5c31fd78b288f577
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-23 03:33:57 +0700

    membuat ulang file KrsApp

--- app/src/main/java/com/example/ucp2/KrsApp.kt
+++ app/src/main/java/com/example/ucp2/KrsApp.kt
@@ -1,8 +1,7 @@
-package com.example.ucp2.ui
+package com.example.ucp2
 
 import android.app.Application
 import com.example.ucp2.dependenciesinjection.ContainerApp
-
 
 class KrsApp : Application() {
     lateinit var containerApp: ContainerApp
