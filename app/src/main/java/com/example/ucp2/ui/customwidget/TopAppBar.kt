29afdd5d5cc995b2ef7b6bc93367cb06cec91e31
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 23:06:23 +0700

    Membuat komponen TopAppBar untuk  tombol kembali dan judul

--- app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -1,2 +1,52 @@
 package com.example.ucp2.ui.customwidget
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(
+    onBack: () -> Unit,
+    showBackButton: Boolean = true,
+    judul: String,
+    modifier: Modifier = Modifier
+) {
+    SmallTopAppBar(
+        title = {
+            Text(
+                text = judul,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xff030303)
+            )
+        },
+        navigationIcon = {
+            if (showBackButton) {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Kembali",
+                        tint = Color(0xFF843181) // Crimson icon
+                    )
+                }
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Color(0xFFF9F9F9),
+            navigationIconContentColor = Color(0xFF843181),
+            titleContentColor = Color(0xFF030303)
+        ),
+        modifier = modifier
+    )
+}
