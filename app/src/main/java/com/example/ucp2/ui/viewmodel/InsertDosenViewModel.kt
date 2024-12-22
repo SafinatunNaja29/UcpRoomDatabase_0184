a2ff8f91b35f74fea4984f361425328bef9ccf80
SafinatunNaja29 <safinatun.nj29@gmail.com>
2024-12-22 22:36:49 +0700

    menambabhkan data class pada file InsertDosenViewModel.kt

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/InsertDosenViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.viewmodel
+
+import com.example.ucp2.data.entity.Dosen
+
+data class DosenUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackBarMessage: String? = null
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null
+){
+    fun isValid(): Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
+fun DosenEvent.toDosenEntity(): Dosen = Dosen(
+    nidn = nidn,
+    nama = nama,
+    jenisKelamin = jenisKelamin
+)
+
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
